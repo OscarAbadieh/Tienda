070eaa6826ae21ebfc86538cce845a60c9d5a950
UBH-M.Infantil <UBH-M.Infantil@DESKTOP-8OV8UNO>
2021-09-25 22:06:58 -0600

    Creación del login

--- Tienda/win.Tienda/FrmMenu.cs
+++ Tienda/win.Tienda/FrmMenu.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace win.Tienda
 {
-    public partial class Form1 : Form
+    public partial class FrmMenu : Form
     {
-        public Form1()
+        public FrmMenu()
         {
             InitializeComponent();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void FrmMenu_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
